84e18b7616c073601b1eb8c0cf85f57894ea7dc9
Facundo Toffolo Pasquini <f.toff.pas@gmail.com>
2022-08-09 14:30:46 -0300

    Update && dist

---
+++ src/components/index.jsx
@@ -0,0 +1,18 @@
+import Breadcrum from './Breadcrum'
+import Footer from './Footer'
+// import Forms from './Forms'
+import Modal from './Modal'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+import Slideshow from './Slideshow'
+import Spinner from './Spinner'
+
+export {
+    Breadcrum,
+    Footer,
+    Modal,
+    Navbar,
+    Sidebar,
+    Slideshow,
+    Spinner
+}
