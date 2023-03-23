6b8683e17fdc51a2cb9aa1baa157e8a9a8e044cb
Facundo Toffolo Pasquini <facundotoffolo@gmail.com>
2023-03-23 20:26:03 -0300

    0.0.7

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import Breadcrum from './components/Breadcrum.jsx'
+import Footer from './components/Footer.jsx'
+import Spinner from './components/Spinner.jsx'
+import Sidebar from './components/Sidebar.jsx'
+import Modal, {ModalButtonWrapper} from './components/Modal.jsx'
 
-export * from './components/Breadcrum.jsx'
-export * from './components/Footer.jsx'
-export * from './components/Spinner.jsx'
-export * from './components/Sidebar.jsx'
-export * from './components/Modal.jsx'
+export {
+    Breadcrum,
+    Footer,
+    Spinner,
+    Sidebar,
+    Modal,
+    ModalButtonWrapper
+}
