1aed4dd272b137ef22440aedaea871f25e1badd4
Facundo Toffolo Pasquini <facundotoffolo@gmail.com>
2024-08-23 00:25:35 -0300

    Pagina demo (#27)
    * Update README.md
    
    * docs: Added README."en".md translation via https://github.com/dephraiim/translate-readme
    
    * Update Doc
    
    * Update index.js
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import Breadcrumb from './components/Breadcrumb.jsx'
+import Button from './components/Button.jsx'
 import Footer from './components/Footer.jsx'
-import Spinner from './components/Spinner.jsx'
+import Modal, { ModalButtonWrapper } from './components/Modal.jsx'
+import Navbar from './components/Navbar.jsx'
 import Sidebar from './components/Sidebar.jsx'
-import Modal, {ModalButtonWrapper} from './components/Modal.jsx'
+import Slideshow from './components/Slideshow.jsx'
+import Spinner from './components/Spinner.jsx'
 import Table from './components/Table.jsx'
+import TooltipIconLabel from './components/TooltipIconLabel.jsx'
+
 import FormInput from './components/forms/FormInput.jsx'
 import FormSelect from './components/forms/FormSelect.jsx'
-import Navbar from './components/Navbar.jsx'
 
 export {
     Breadcrumb,
+    Button,
     Footer,
-    Spinner,
-    Sidebar,
     Modal,
-    ModalButtonWrapper,
+    Navbar,
+    Sidebar,
+    Slideshow,
+    Spinner,
     Table,
+    TooltipIconLabel,
+    //OTHER
+    ModalButtonWrapper,
+    // FORMS
     FormInput,
     FormSelect,
-    Navbar
 }
